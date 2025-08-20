001479e85e6478eca066cec65e92da5ddc90b087
Wajieh <wajiehbadr@gmail.com>
2025-08-20 20:34:05 +0500

    Frontend recovered, no animation

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from routes.api import api_bp
+from routes.views import views_bp
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
+
+# Register blueprints
+app.register_blueprint(api_bp, url_prefix="/")
+app.register_blueprint(views_bp, url_prefix="/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
