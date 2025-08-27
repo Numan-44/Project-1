150adf1d30ca06fc60d0a1f592220be5afee4320
Wajieh <wajiehbadr@gmail.com>
2025-08-27 20:46:45 +0500

    address change

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ app.register_blueprint(api_bp, url_prefix="/")
 app.register_blueprint(views_bp, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
